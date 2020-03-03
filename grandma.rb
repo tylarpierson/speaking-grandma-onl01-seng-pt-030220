0bf3796f28b64a87dae49b589b146967a37f774f
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-03 17:36:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,15 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "Hi Nana, how are you?"
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi!"
-   puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-   puts "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
-    puts "NO, NOT SINCE 1938!"
-  else 
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
+    elsif phrase
   end 
 end 
