f6c106f55173a2090f69d4d908e031c1bea19408
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-03 17:37:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN"
   elsif phrase == phrase.upcase
-    puts ""
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end 
 end 
